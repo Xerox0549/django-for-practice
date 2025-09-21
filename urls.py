cb3c0a5b2c209ac403704249dc1c7d8563ba5ea3
Pushpendra singh <pushpendrasingh0549@gmail.com>
2025-09-21 07:40:07 +0530

    Update file

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 def home(request):
     return HttpResponse("🚀 Django Docker Demo is running!")
+    return HttpResponse("And back to home page")
 
 urlpatterns = [
     path("", home),
